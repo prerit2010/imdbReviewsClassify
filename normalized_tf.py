05cedb81b48a3964698ceb4e9d2191978fc890eb
Prerit Garg <prerit2010@gmail.com>
2018-03-13 16:57:37 +0530

    Performance improvement for division in sparse matrix

--- normalized_tf.py
+++ normalized_tf.py
@@ -7,6 +7,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 import time
 from scipy import sparse
+from scipy.sparse import csr_matrix
 from sklearn.preprocessing import normalize
 
 test_bag_of_words = load_svmlight_file("aclImdb/test/labeledBow.feat")
@@ -23,8 +24,27 @@ counts_test = np.array([ i.count_nonzero() for i in X_test ])
 print("Counts completed in %d Seconds" % int(time.time()-start_time))
 
 start_time = time.time()
-X_train = X_train / counts_train[:,None]
-X_test = X_test / counts_test[:,None]
+r,c = X_train.nonzero()
+val = np.repeat(1.0/counts_train, X_train.getnnz(axis=1))
+rD_sp = csr_matrix((val, (r,c)), shape=(X_train.shape))
+X_train = X_train.multiply(rD_sp)
+
+r,c = X_test.nonzero()
+val = np.repeat(1.0/counts_test, X_test.getnnz(axis=1))
+rD_sp = csr_matrix((val, (r,c)), shape=(X_test.shape))
+X_test = X_test.multiply(rD_sp)
+
+# r,c = X_train.nonzero()
+# rD_sp = csr_matrix(((1.0/counts_train)[r], (r,c)), shape=(X_train.shape))
+# X_train = X_train.multiply(rD_sp)
+
+# r,c = X_test.nonzero()
+# rD_sp = csr_matrix(((1.0/counts_test)[r], (r,c)), shape=(X_test.shape))
+# X_test = X_test.multiply(rD_sp)
+
+
+# X_train = X_train / counts_train[:,None]
+# X_test = X_test / counts_test[:,None]
 print("Divisions completed in %d Seconds" % int(time.time()-start_time))
 
 start_time = time.time()
@@ -40,8 +60,8 @@ print("Transformation completed in %d Seconds" % int(time.time()-start_time))
 # print(type(X_train))
 # print(X_train.shape)
 print("Training...")
-clf = BernoulliNB()
-# clf = LogisticRegression()
+# clf = BernoulliNB()
+clf = LogisticRegression()
 # clf = svm.SVC(max_iter=1000)
 # clf = MLPClassifier(learning_rate_init=0.001, verbose=True, max_iter=50, hidden_layer_sizes=(20, ))
 start_time = time.time()
